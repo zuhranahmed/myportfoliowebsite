84c30b1e1163f3cd65b52e6b50b124272aa289e7
zuhranahmed <zuhranahmed@gmail.com>
2020-12-21 15:37:50 +0500

    Add about file

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,19 @@
 const burger = document.querySelector(".burger");
 const navItems = document.querySelector(".nav-items");
+const container = document.querySelector(".wrapper");
+const overlay = document.querySelector(".overlay");
 
 // responsive navbar
 const navSlide = () => {
   burger.addEventListener("click", () => {
     navItems.classList.toggle("nav-active");
+    overlay.classList.toggle("hidden");
   });
 };
+
+overlay.addEventListener("click", function () {
+  navItems.classList.remove("nav-active");
+  overlay.classList.add("hidden");
+});
 
 navSlide();
