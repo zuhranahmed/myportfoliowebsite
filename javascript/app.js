0be2ca43a5d939284c2a36846ca54aa7ae8bd541
zuhranahmed <zuhranahmed@gmail.com>
2020-12-22 16:52:38 +0500

    testing commit

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,8 @@ const burger = document.querySelector(".burger");
 const navItems = document.querySelector(".nav-items");
 const container = document.querySelector(".wrapper");
 const overlay = document.querySelector(".overlay");
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
 
 // responsive navbar
 const navSlide = () => {
@@ -17,3 +19,26 @@ overlay.addEventListener("click", function () {
 });
 
 navSlide();
+
+// Navbar Fade Animation
+
+const hoverNavItems = function (e, opacity) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const navItems = link.closest(".navbar").querySelectorAll(".nav-link");
+
+    navItems.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+navbar.addEventListener("mouseover", function (e) {
+  hoverNavItems(e, 0.5);
+});
+navbar.addEventListener("mouseout", function (e) {
+  hoverNavItems(e, 1);
+});
